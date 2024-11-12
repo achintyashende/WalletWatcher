3afc6b69c6bd7e221e379711461dbb92e88ddd5a
Achintya Shende <achintyashende@gmail.com>
2024-11-13 03:22:30 +0530

    Update Backend Port in utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -12,4 +12,5 @@ export const handleError = (msg) => {
     })
 }
 
-export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+// export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+export const APIUrl = "https://walletwatcher-backend.onrender.com";
